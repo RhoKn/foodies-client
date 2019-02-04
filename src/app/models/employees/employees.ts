eeff59a87e83d68f41c8bc2c6e3ea2cf1bee720b
Ruben Gonzalez <rubengonzalez@MacBook-Pro-de-Ruben.local>
2019-02-03 17:19:36 -0700

    Se realizan los modelos para usuarios y empleados

---
+++ src/app/models/employees/employees.ts
@@ -0,0 +1,11 @@
+export class Employee {
+    constructor(
+        public firstName: String,
+        public lastName: String,
+        public age: Number,
+        public role: String,
+        public shift: String,
+        public weeklyHours: Number,
+        public registrationDate: String
+    ) {}
+}
